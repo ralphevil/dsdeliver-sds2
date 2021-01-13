5a09f201c0003921a162418427300f0ac96655d5
Raphael Souza <ralphevils5@gmail.com>
2021-01-13 18:51:57 -0300

    Add Orders List

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -2,9 +2,8 @@ import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fo
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
 import AppLoading from 'expo-app-loading';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,9 +17,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
-      <Home/>
       <StatusBar style="auto" />
+      <Routes/>
     </View>
   );
 }
